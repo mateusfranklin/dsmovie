9097a1b5774e72473a3b695096f97217ed6eb35f
Mateus Franklin <contato@mateusfranklin.com>
2022-01-14 13:20:51 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './style.css';
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                <h1>DSMovie</h1>
+                <a href="https://github.com/mateusfranklin">
+                    <div className="dsmovie-contact-container">
+                    <GithubIcon />
+                    <p className="dsmovie-contact-link">/mateusfranklin</p>
+                    </div>
+                </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
